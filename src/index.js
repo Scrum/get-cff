76a1f9717a7b6e719cd3ee087fb750a7ebc44446
Ivan Demidov <scrum@list.ru>
2017-11-21 17:10:49 +0300

    feat: add read methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-export default options => {
-	return new Promise(resolve => {
-		resolve(options);
+import fs from 'fs';
+import flatry from 'flatry';
+
+const methods = [
+	path => require(path),
+	path => JSON.parse(fs.readFileSync(path, 'utf-8'))
+];
+
+export default path => new Promise((resolve, reject) => {
+	let err;
+	let res;
+	methods.some(method => {
+		[err, res] = flatry(() => method(path));
+		return res;
 	});
-};
+
+	if (err) {
+		reject(err);
+	}
+
+	resolve(res);
+});
