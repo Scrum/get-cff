8fce7aec047626ca54811ef349dfd01ffb42e101
Ivan Demidov <scrum@list.ru>
2017-11-21 17:08:25 +0300

    test: add tests for three type file

--- test/get-cff.spec.js
+++ test/get-cff.spec.js
@@ -1,6 +1,25 @@
+import path from 'path';
 import isPromise from 'is-promise';
 import getCff from '../src/index';
 
 test('processing should return promise', () => {
 	expect(isPromise(getCff())).toBeTruthy();
+});
+
+test('processing should return data from js file', async () => {
+	const file = path.resolve('test/files/jsFile.js');
+	const config = await getCff(file);
+	expect(config.property).toBe('value');
+});
+
+test('processing should return data from json file', async () => {
+	const file = path.resolve('test/files/jsonFile.json');
+	const config = await getCff(file);
+	expect(config.property).toBe('value');
+});
+
+test('processing should return data from dot file', async () => {
+	const file = path.resolve('test/files/.dotFile');
+	const config = await getCff(file);
+	expect(config.property).toBe('value');
 });
